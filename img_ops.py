021180bb49f8914acae67ddd9d731ddb8ba42532
DJV760 <150827760+DJV760@users.noreply.github.com>
2023-12-20 07:41:24 +0100

    Delete img_ops.py

--- img_ops.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-Image.MAX_IMAGE_PIXELS = None
-
-original_image = Image.open('C:\\Users\\z0224841\\Downloads\\GP3_orto_A3.png')
-white_image = Image.new('RGB', original_image.size, 'white')
-white_image.save('C:\\Users\\z0224841\\PycharmProjects\\SemWeb\\mask_foundation.png')
